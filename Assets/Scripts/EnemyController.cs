7bb2a4927a2fb921fa6095ca04f36e76cb62a935
qvirin <etoropchin@gmail.com>
2019-05-10 22:42:52 +0300

    enemy ai

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+    public float speed;
+    public Transform groundDetection;
+    public float distance;
+    private bool movingRight = true;
+    private Rigidbody2D rb2d;
+    
+    
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+      
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb2d.velocity = Vector2.right * speed;
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+
+            }
+        }
     }
 }
